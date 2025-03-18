4439fd461f5ed777e26b363dce05a52cab2f4a83
Angel751658 <146266447+Angel751658@users.noreply.github.com>
2025-03-18 14:57:51 -0600

    2:57

--- P_02/index.js
+++ P_02/index.js
@@ -2,13 +2,6 @@ import { Product } from './products.js';
 import './data_handler.js';
 import { ShoppingCart } from './shopping_cart.js';
 import { createProduct, getProducts, deleteProduct, updateProduct, findProduct, productListToHTML } from './data_handler.js';
-// Mostrar productos en el contenedor
 
-// Crear algunos productos de prueba
-const producto1 = new Product("Manzana", "Frescas y deliciosas", "img/manzana.jpg", "kg", 20, 30, "Fruta");
-const producto2 = new Product("Laptop", "Poderosa para tus tareas", "img/laptop.jpg", "unidad", 5, 15000, "Electrónica");
-
-createProduct(producto1);
-createProduct(producto2);
 
 
