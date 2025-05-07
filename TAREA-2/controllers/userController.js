245c1d9c3e4a4a1223e9ff35d283ecbf508ad3cd
Angel751658 <146266447+Angel751658@users.noreply.github.com>
2025-05-07 15:02:06 -0600

    3:02

--- TAREA-2/controllers/userController.js
+++ TAREA-2/controllers/userController.js
@@ -2,49 +2,49 @@ const User = require('../models/user');
 
 // Creacion
 exports.createUser = async (req, res) => {
-  try {
-    const { name, email, password, age } = req.body;
-    const user = new User({ name, email, password, age });
-    await user.save();
-    res.status(201).json(user);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
+    try {
+        const { name, email, password, age } = req.body;
+        const user = new User({ name, email, password, age });
+        await user.save();
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 };
 
 // Leectura
 exports.getAllUsers = async (req, res) => {
-  const users = await User.find();
-  res.json(users);
+    const users = await User.find();
+    res.json(users);
 };
 exports.getUserByEmail = async (req, res) => {
-  const user = await User.findOne({ email: req.params.email });
-  user ? res.json(user) : res.status(404).json({ error: 'No encontrado' });
+    const user = await User.findOne({ email: req.params.email });
+    user ? res.json(user) : res.status(404).json({ error: 'No encontrado' });
 };
 
 // Actualizacion
 exports.updateUserByEmail = async (req, res) => {
-  try {
-    const updatedUser = await User.findOneAndUpdate(
-      { email: req.params.email },
-      req.body,
-      { new: true }
-    );
-    updatedUser
-      ? res.json(updatedUser)
-      : res.status(404).json({ error: 'No encontrado' });
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
+    try {
+        const updatedUser = await User.findOneAndUpdate(
+            { email: req.params.email },
+            req.body,
+            { new: true }
+        );
+        updatedUser ? res.json(updatedUser) : res.status(404).json({ error: 'No encontrado' });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 };
 
 // Eliminacion
 exports.deleteUserByEmail = async (req, res) => {
-  const deletedUser = await User.findOneAndDelete({ email: req.params.email });
-  deletedUser
-    ? res.json({ message: 'Usuario eliminado' })
-    : res.status(404).json({ error: 'No encontrado' });
+    const deletedUser = await User.findOneAndDelete({ email: req.params.email });
+    deletedUser ? res.json({ message: 'Usuario eliminado' }) : res.status(404).json({ error: 'No encontrado' });
 };
+
+
+
+
 
 
 
